1d8fce0ad4024a4780490af650eba6e3c75d7e0e
Xuan Hieu <39899509+xuanhieule@users.noreply.github.com>
2020-06-25 19:23:16 +0700

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -1,3 +1,126 @@
+
+function addRow(tableId, name, size, ploc, lloc, blank, comment) {
+    var row = tableId.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2)
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var nameText = document.createTextNode(name);
+    var sizeText = document.createTextNode(size);
+    var plocText = document.createTextNode(ploc);
+    var llocText = document.createTextNode(lloc);
+    var blankText = document.createTextNode(blank);
+    var commentText = document.createTextNode(comment);
+    cell1.appendChild(nameText);
+    cell2.appendChild(sizeText);
+    cell3.appendChild(plocText);
+    cell4.appendChild(llocText);
+    cell5.appendChild(blankText);
+    cell6.appendChild(commentText);
+}
+
+document.querySelector('input').addEventListener("change", function (event) {
+    var table = document.getElementById("myTable");
+    var lines;
+    var fileArr = event.target.files;
+    var java = /(\.java)/;
+    var cShap = /(\.cs)/;
+    var not = /(\.csv)/;
+    var not1 = /(\.css)/
+    var cPlusPlus = /(\.cpp)/;
+    var text = /(\.txt)/;;
+    var nameArr = [];
+    var sizeArr = [];
+    var temp = 0;
+
+    for (var i = 0; i < fileArr.length; i++) {
+        var blank = 0;
+        var comment = 0;
+        var logic = 0;
+        var block = 0;
+        var ploc = 0;
+        var temp2 = 0;
+
+
+        if (not.exec(fileArr[i].name) || not1.exec(fileArr[i].name)) { }
+        else
+            if (java.exec(fileArr[i].name) || cShap.exec(fileArr[i].name) || cPlusPlus.exec(fileArr[i].name) || text.exec(fileArr[i].name)) {
+            nameArr.push(fileArr[i].name);
+            sizeArr.push(fileArr[i].size + " byte");
+            const reader = new FileReader();
+            reader.readAsText(fileArr[i]);
+            reader.onload = function (e) {
+                lines = e.target.result.split(/\r\n|\n/);
+                //đếm các dong trống
+                for (x of lines) {
+                    if (x.trim() === '') {
+                        blank++;
+                    }
+                }
+                //đếm comment line
+                for (x of lines) {
+                    if (x.indexOf('//') !== -1) {
+                        // console.log('-----------------------------------------------------------');
+                        // console.log(x);
+                        comment++;
+                    }
+                }
+                for (var i = 0; i < lines.length; i++) {
+                    if (lines[i].indexOf('/*') !== -1) {
+                        for (var j = i; j < lines.length; j++) {
+                            if (lines[j].indexOf('*/') !== -1) {
+                                comment++;
+                                i = j;
+                                // console.log('-----------------------------------------------------------');
+                                // console.log(lines[j]);
+                                break;
+                            }
+                            else
+                                if (lines[j].charAt(lines[j].length - 1) === ";") {
+                                    logic--;
+                                }
+                            // console.log('-----------------------------------------------------------');
+                            // console.log(lines[j]);
+                            comment++;
+                        }
+                    }
+                }
+                // console.log("comment: " + comment);
+
+                // đếm các lệnh có ;
+                for (x of lines) {
+                    if (x.charAt(x.length - 1) === ";") {
+                        // console.log(x);
+                        // console.log('-------------------------------------------');
+                        logic++;
+                    }
+                    for (var a = 0; a < x.length - 1; a++) {
+                        if (x.charAt(a) === ";") {
+                            console.log(x);
+                            temp2++;
+                        }
+                    }
+                }
+                console.log("temp:" + temp2);
+                logic = lines.length - blank - comment + temp2;
+                addRow(table, nameArr[temp], sizeArr[temp], lines.length, logic, blank, comment);
+                blank = 0;
+                comment = 0;
+                logic = 0;
+                block = 0;
+                ploc = 0;
+                temp++;
+
+            }
+
+        }
+    }
+
+
+}, false);
+
 // JS FRONTEND
 document.addEventListener("DOMContentLoaded", function () {
     var btn = document.querySelector('.border-me');
@@ -18,152 +141,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 }, false);
 
-function addRow(tableId, name, size, loc, ploc, lloc, comment) {
-    var row = tableId.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
-    var nameText = document.createTextNode(name);
-    var sizeText = document.createTextNode(size);
-    var locText = document.createTextNode(loc);
-    var plocText = document.createTextNode(ploc);
-    var llocText = document.createTextNode(lloc);
-    var commentText = document.createTextNode(comment);
-    cell1.appendChild(nameText);
-    cell2.appendChild(sizeText);
-    cell3.appendChild(locText);
-    cell4.appendChild(plocText);
-    cell5.appendChild(llocText);
-    cell6.appendChild(commentText);
-}
-//js Upload
-document.querySelector('input').addEventListener("change", function (event) {
-    var table = document.getElementById("myTable");
-    var lines;
-    var fileArr = event.target.files;
-    var java = /(\.java)/;
-    var cShap = /(\.cs)/;
-    var not = /(\.csv)/;
-    var not1 = /(\.css)/
-    var cPlusPlus = /(\.cpp)/;
-    var text = /(\.txt)/;;
-    var nameArr = [];
-    var sizeArr = [];
-    var temp = 0;
-
-    for (var i = 0; i < fileArr.length; i++) {
-        var linenull = 0;
-        var comment = 0;
-        var logic = 0;
-        var block = 0;
-        var ploc = 0;
-        if (not.exec(fileArr[i].name) || not1.exec(fileArr[i].name)) { }
-        else
-            if (java.exec(fileArr[i].name) || cShap.exec(fileArr[i].name) || cPlusPlus.exec(fileArr[i].name) || text.exec(fileArr[i].name)) {
-                nameArr.push(fileArr[i].name);
-                sizeArr.push(fileArr[i].size + " byte");
-                const reader = new FileReader();
-                reader.readAsText(fileArr[i]);
-                reader.onload = function (e) {
-                    lines = e.target.result.split(/\r\n|\n/);
-                    //đếm các dong trống
-                    for (x of lines) {
-                        if (x.trim() === '') {
-                            linenull++;
-                        }
-                    }
-                    //đếm comment line
-                    for (x of lines) {
-                        if (x.indexOf('//') !== -1) {
-                            // console.log('-----------------------------------------------------------');
-                            // console.log(x);
-                            comment++;
-                        }
-                    }
-                    for (var i = 0; i < lines.length; i++) {
-                        if (lines[i].indexOf('/*') !== -1) {
-                            for (var j = i; j < lines.length; j++) {
-                                if (lines[j].indexOf('*/') !== -1) {
-                                    comment++;
-                                    i = j;
-                                    // console.log('-----------------------------------------------------------');
-                                    // console.log(lines[j]);
-                                    break;
-                                }
-                                else
-                                    if (lines[j].charAt(lines[j].length - 1) === ";") {
-                                        logic--;
-                                    }
-                                // console.log('-----------------------------------------------------------');
-                                // console.log(lines[j]);
-                                comment++;
-                            }
-                        }
-                    }
-                    // console.log("comment: " + comment);
-
-                    // đếm các lệnh có ;
-                    for (x of lines) {
-                        if (x.charAt(x.length - 1) === ";") {
-                            // console.log(x);
-                            // console.log('-------------------------------------------');
-                            logic++;
-                        }
-                        for (var a = 0; a < x.length - 1; a++) {
-                            if (x.charAt(a) === ";") {
-
-                                logic++;
-                            }
-                        }
-                        // for (var k = 0; k < x.length-1; k++) {
-                        //     if (x.charAt(k) === '"' || x.charAt(k) === "'") {
-                        //          for (var l = k + 1; l < x.length-1; l++) {
-                        //             if (x.charAt(l) === '"' || x.charAt(l) === "'") {
-                        //                 console.log(x.charAt());
-                        //                 for (var h = l - 1; h >= 1; h--) {
-                        //                     if (x.charAt(h) === ";") {
-                        //                         logic--;
-                        //                     }
-                        //                }
-                        //              }
-                        //          }
-                        //     }
-                        // }
-                    }
-                    // console.log('---------------------------------------------');
-                    // console.log("lenh co ; : " + logic);
-                    //đếm các khối lệnh
-                    for (x of lines) {
-                        if (x.indexOf('{') !== -1 && x.indexOf('//') === -1) {
-                            // console.log(x);
-                            console.log('-----------------------------------------------------------------');
-                            block++;
-
-                        }
-                    }
-                    logic = logic + block;
-                    ploc = lines.length - linenull - comment;
-                    // console.log("block: " + block);
-                    // console.log("********************************************************************************************");
-                    // console.log("LOC: " + lines.length);
-                    // console.log("PLOC: " + ploc);
-                    // console.log("LOGIC: " + logic);
-                    // console.log("COMMENT: " + comment);
-                    // console.log("*********************************************************************************");
-                    // console.log(nameArr[temp]);
-                    // console.log(sizeArr[temp]);
-                    // console.log(temp);
-                    addRow(table, nameArr[temp], sizeArr[temp], lines.length, ploc, logic, comment);
-                    linenull = 0;
-                    comment = 0;
-                    logic = 0;
-                    block = 0;
-                    ploc = 0;
-                    temp++;
-                }
-            }
+document.addEventListener("DOMContentLoaded",function () {
+    var btnreset = document.querySelector('.btnreset');
+    btnreset.onclick = function(){
+        location.reload();
     }
-}, false);
+},false);
